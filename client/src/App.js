92c223c2596af51661b69b0741c0f3f1645fcdd9
Nokukhanya Sidu <4205@kevro.co.za>
2023-10-20 11:49:50 +0200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navigation from './components/navigation';
+
+function App() {
+  return (
+    <div className="App">
+      < Navigation/>
+      <header className="App-header">
+   
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/navigation.js
@@ -0,0 +1,52 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Noxy from './images/Noxy.png'
+
+function Navigation(){
+    return(
+        <>
+         {[false].map((expand) => (
+        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">
+            <img src={Noxy} alt="Logo" width={'50px'} />
+                </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">Link</Nav.Link>
+                 
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+        </>
+    )
+}
+export default Navigation;
